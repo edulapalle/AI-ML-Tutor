bd06da274dc1077c7517656f96cb4da18ce6d913
Zach Wilson <zachwilson130@gmail.com>
2025-07-17 21:40:48 +0000

    adding all

---
+++ api/index.py
@@ -0,0 +1,10 @@
+from fastapi import FastAPI
+from fastapi.responses import JSONResponse
+from mangum import Mangum
+app = FastAPI()
+
+@app.get("/")
+def read_root():
+    return JSONResponse(content={"message": "Hello from FastAPI on Vercel!"})
+
+handler = Mangum(app)
